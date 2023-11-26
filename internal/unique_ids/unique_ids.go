31bb9b9e55f582b5f3c91ce02034f7cbc6697a2e
Lyndon Shi <shilyndon@outlook.com>
2023-11-26 13:58:45 -0800

    channel for ids

--- internal/unique_ids/unique_ids.go
+++ internal/unique_ids/unique_ids.go
@@ -1,1 +1,48 @@
 package unique_ids
+
+import (
+	"context"
+	"errors"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func AddUniqueIdsHandle(ctx context.Context, n *maelstrom.Node) {
+	counter := make(chan int, 1)
+
+	go func() {
+		i := 0
+		select {
+		case _, ok := <-ctx.Done():
+			if !ok {
+				close(counter)
+				return
+			}
+		case counter <- i:
+			i++
+		}
+	}()
+
+	n.Handle("generate", uniqueIdsBuilder(n, counter))
+}
+
+func uniqueIdsBuilder(n *maelstrom.Node, counter chan int) maelstrom.HandlerFunc {
+	unique_ids := func(msg maelstrom.Message) error {
+		node_id := n.ID()
+		id, ok := <-counter
+		if !ok {
+			return errors.New("Counter channel closed")
+		}
+
+		// Unmarshal the message body as an loosely-typed map.
+		resp := make(map[string]any)
+
+		// Update the message type to return back.
+		resp["type"] = "generate_ok"
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, resp)
+	}
+
+	return unique_ids
+}
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"context"
 	"gossip-glomers/internal/echo"
+	"gossip-glomers/internal/unique_ids"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	defer ctxCancel()
+
 	n := maelstrom.NewNode()
 	echo.AddEchoHandle(n)
+	unique_ids.AddUniqueIdsHandle(ctx, n)
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
