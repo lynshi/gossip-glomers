986ce6e053311c74f57bb81cf3c130eba6ba510e
Lyndon Shi <shilyndon@outlook.com>
2023-11-26 19:57:22 -0800

    pkg/errors

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gossip-glomers
 
 go 1.20
 
-require github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231125184606-25bd4e47b791
+require (
+	github.com/jepsen-io/maelstrom/demo/go v0.0.0-20231125184606-25bd4e47b791
+	github.com/pkg/errors v0.9.1
+)
--- internal/unique_ids/unique_ids.go
+++ internal/unique_ids/unique_ids.go
@@ -2,11 +2,10 @@ package unique_ids
 
 import (
 	"context"
-	"errors"
-	"fmt"
 	"strconv"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+	"github.com/pkg/errors"
 )
 
 func AddUniqueIdsHandle(ctx context.Context, n *maelstrom.Node) {
@@ -34,7 +33,7 @@ func uniqueIdsBuilder(n *maelstrom.Node, counter chan uint32) maelstrom.HandlerF
 		node_id := n.ID()
 		node_id_num, err := strconv.Atoi(node_id[1:])
 		if err != nil {
-			return fmt.Errorf("could not get integer from %s", node_id)
+			return errors.Wrapf(err, "could not get integer from %s", node_id)
 		}
 
 		id, ok := <-counter
