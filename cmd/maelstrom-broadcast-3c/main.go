18013603c901e00124b181b1abdd2e585fc375b2
Lyndon Shi <shilyndon@outlook.com>
2023-11-28 15:08:14 -0800

    move to 3c

---
+++ cmd/maelstrom-broadcast-3c/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"gossip-glomers/internal/broadcast"
+	"log"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	defer ctxCancel()
+
+	maelstrom_node := maelstrom.NewNode()
+
+	broadcast_node := broadcast.NewFaultTolerantNode(ctx, maelstrom_node)
+	defer broadcast_node.ShutdownFaultTolerantNode()
+
+	broadcast_node.AddBroadcastHandle(maelstrom_node)
+	broadcast_node.AddBroadcastRepeatHandle(maelstrom_node)
+	broadcast_node.AddReadHandle(maelstrom_node)
+	broadcast_node.AddTopologyHandle(maelstrom_node)
+
+	if err := maelstrom_node.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
